dff7c29cab0e3f4fe92ba1eb097d8a7ecdf00a3f
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2019-10-07 15:51:08 +0300

    Add project files.

---
+++ Epam_Trainings_Homework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Epam_Trainings_Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
