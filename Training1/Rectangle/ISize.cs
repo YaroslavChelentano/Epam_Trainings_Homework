da0212f934c75cd2c4d1fb6f48d8ef6d09741134
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2019-10-08 09:15:01 +0300

    Add folders for each task and add interface ISize

---
+++ Training1/Rectangle/ISize.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training1.Rectangle
+{
+    public interface ISize
+    {
+        int Width { get; set; }
+        int Height { get; set; }
+        int Perimeter(int width, int height);
+    }
+}
