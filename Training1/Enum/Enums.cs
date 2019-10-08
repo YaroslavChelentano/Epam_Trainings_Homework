64f400c6665147feecce58298c0cda1106a77d55
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2019-10-08 09:52:14 +0300

    Add enum of months

---
+++ Training1/Enum/Enums.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Training1.Enum
+{
+    enum Month
+    {
+        January,
+        February,  
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+}
