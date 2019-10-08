888177b6bc4d2da8bb9ea3403278fa03dd7bf645
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2019-10-08 08:58:55 +0300

    Add method that display Age of Person

--- Training1/Person.cs
+++ Training1/Person.cs
@@ -4,6 +4,28 @@ namespace Training1
 {
     public struct Person
     {
+        public string Name;
+        public string Surname;
+        public int Age;
+
+        public void DisplayAgeOfPerson(int n)
+        {
+            if (n > 0)
+            {
+                if (Age > n)
+                {
+                    Console.WriteLine($"{Name} {Surname} older than {n}");
+                }
+                else
+                {
+                    Console.WriteLine($"{Name} {Surname} younger than {n}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Argument n is not valid");
+            }
+        }
 
     }
 }
