6df0d1fc1d9636e0829a2d5d11386a2a691d5c65
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2019-10-11 11:28:29 +0300

    Add menu to main

--- Epam_Trainings_Homework/Program.cs
+++ Epam_Trainings_Homework/Program.cs
@@ -11,38 +11,53 @@ namespace Epam_Trainings_Homework
     {
         static void Main(string[] args)
         {
-            // Task 1
-            var Jerry = new Person() { Name = "Jerry", Surname = "Koval", Age = 17 };
-            Console.WriteLine("Enter Age of Person: ");
-            var ageToComparison = int.Parse(Console.ReadLine());
-            Jerry.DisplayAgeOfPersonInOrderToGivenNumber(ageToComparison);
-            // Task 2
-            Console.WriteLine("Enter Width of Rectangle: ");
-            var width = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter Heigth of Rectangle: ");
-            var height = double.Parse(Console.ReadLine());
-            var rectangle = new Rectangle(width, height);
-            Console.WriteLine($"Perimeter of Rectangle is {rectangle.Perimeter()}");
-            // Task 3
-            Console.WriteLine("Enter number of Month: ");
-            int numberOfMonth = int.Parse(Console.ReadLine());
-            if (0 <= numberOfMonth && numberOfMonth <= 12)
+            Console.WriteLine("Choose training: /n 1.Struct /n 2.Exceptions");
+            int choiceTraining = int.Parse(Console.ReadLine());
+            switch (choiceTraining)
             {
-                string selectedMonth = Enum.GetName(typeof(Month), numberOfMonth - 1);
-                Console.WriteLine($"Selected Month is {selectedMonth} ");
+                case 1:
+                    // Task 1
+                    var Jerry = new Person() { Name = "Jerry", Surname = "Koval", Age = 17 };
+                    Console.WriteLine("Enter Age of Person: ");
+                    var ageToComparison = int.Parse(Console.ReadLine());
+                    Jerry.DisplayAgeOfPersonInOrderToGivenNumber(ageToComparison);
+                    // Task 2
+                    Console.WriteLine("Enter Width of Rectangle: ");
+                    var width = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter Heigth of Rectangle: ");
+                    var height = double.Parse(Console.ReadLine());
+                    var rectangle = new Rectangle(width, height);
+                    Console.WriteLine($"Perimeter of Rectangle is {rectangle.Perimeter()}");
+                    // Task 3
+                    Console.WriteLine("Enter number of Month: ");
+                    int numberOfMonth = int.Parse(Console.ReadLine());
+                    if (0 <= numberOfMonth && numberOfMonth <= 12)
+                    {
+                        string selectedMonth = Enum.GetName(typeof(Month), numberOfMonth - 1);
+                        Console.WriteLine($"Selected Month is {selectedMonth} ");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid value number of Month");
+                    }
+                    // Task 4
+                    Extensions.DisplayEnum();
+                    Extensions.DisplaySortedDictionary();
+                    // Task 5
+                    long maxValueOfLong = (long)LongRange.Max;
+                    long minValueOfLong = (long)LongRange.Min;
+                    Console.WriteLine($"Max = {maxValueOfLong}");
+                    Console.WriteLine($"Min = {minValueOfLong}");
+                    break;
+                case 2:
+                    // Training 2 
+                    // Task 1
+
+                    break;
+                default:
+                    Console.WriteLine("Invalid input value");
+                    break;
             }
-            else
-            {
-                Console.WriteLine("Invalid value number of Month");
-            }
-            // Task 4
-            Extensions.DisplayEnum();
-            Extensions.DisplaySortedDictionary();
-            // Task 5
-            long maxValueOfLong = (long)LongRange.Max;
-            long minValueOfLong = (long)LongRange.Min;
-            Console.WriteLine($"Max = {maxValueOfLong}");
-            Console.WriteLine($"Min = {minValueOfLong}");
         }
     }
 }
