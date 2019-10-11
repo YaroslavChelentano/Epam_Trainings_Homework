ada2753c8602dd7faae0b7192142ace5722aa04c
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2019-10-11 16:11:46 +0300

    add project to training3

---
+++ Training3/Directories.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+namespace Training3
+{
+    public class Directories
+    {
+        DirectoryInfo dir = new DirectoryInfo(@"D:\Навчання");
+        public static void DisplayDirectory()
+        {
+            Console.WriteLine(dir);
+        }
+    }
+}
