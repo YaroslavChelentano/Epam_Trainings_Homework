43a3a6ba29e7ec9a9d590f678bf45cf884eadf06
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2019-10-15 15:36:28 +0300

    Add logger dll

---
+++ Logger/ConsoleLoggerClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Logger
+{
+    class ConsoleLoggerClass : ILogger
+    {
+        public void WriteLog(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        public void ReadLog(string message)
+        {
+
+        }
+    }
+}
---
+++ Logger/FileLoggerClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Logger
+{
+    class FileLoggerClass : ILogger
+    {
+        public readonly string filePath = @"D:\Навчання\Програмування\git\YaroslavChelentano\Epam_Trainings_Homework\Training3\logs.txt";
+        public void WriteLog(string message)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(filePath))
+            {
+                streamWriter.WriteLine(message);
+                streamWriter.Close();
+            }
+        }
+        public void ReadLog(string message)
+        {
+            using (StreamReader sr = new StreamReader(filePath, Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+    }
+}
