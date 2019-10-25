249af3106aaae76986148d32a45890160b8e1a06
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2019-10-25 12:15:21 +0300

    Add path to log file in config.json

--- Logger/FileLoggerClass.cs
+++ Logger/FileLoggerClass.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -7,9 +8,19 @@ namespace Logger
 {
     public class FileLoggerClass : ILogger
     {
-        public readonly string filePath = (@"D:\Навчання\Програмування\git\YaroslavChelentano\Epam_Trainings_Homework\Logger\logArchive.txt");
+        public IConfigurationRoot GetConfigurationOfJson()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("config.json")
+                .Build();
+            return config;
+        }
+
+        public string filePath { get; set; }
+        //(@"D:\Навчання\Програмування\git\YaroslavChelentano\Epam_Trainings_Homework\Logger\logArchive.txt");
         public void WriteLog(string message)
         {
+            Console.WriteLine(filePath);
             using (StreamWriter streamWriter = new StreamWriter(filePath))
             {
                 streamWriter.WriteLine(message);
