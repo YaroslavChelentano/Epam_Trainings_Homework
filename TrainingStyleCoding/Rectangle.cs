9534b66e1347d186f4a72c62b43146e976d177ac
YaroslavChelentano <yaroslav.chelentano@gmail.com>
2019-11-02 18:10:47 +0200

    Add some properties

--- TrainingStyleCoding/Rectangle.cs
+++ TrainingStyleCoding/Rectangle.cs
@@ -4,11 +4,16 @@ namespace TrainingStyleCoding
 {
     public class Rectangle
     {
+        // Coordinates
         public double LeftX { get; set; }
         public double RightX { get; set; }
         public double DownY { get; set; }
         public double UpX { get; set; }
-
+        // Sides of rectangle ABCD
+        public double AB { get; set; }
+        public double BC { get; set; }
+        public double CD { get; set; }
+        public double AD { get; set; }
         public Rectangle (double leftx, double rightx,double downy, double upx)
         {
             LeftX = leftx;
@@ -16,6 +21,8 @@ namespace TrainingStyleCoding
             DownY = downy;
             UpX = upx;
         }
+
+
 
 
     }
