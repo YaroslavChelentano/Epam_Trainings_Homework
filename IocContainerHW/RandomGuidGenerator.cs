77d891ee9adc393f01ec91a4dbcd5ec880430a35
Ярослав Андреєв <yaroslav.chelentano@gmail.com>
2019-12-16 17:38:02 +0200

    Add service one and random generator guid

---
+++ IocContainerHW/RandomGuidGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IocContainerHW
+{
+    public class RandomGuidGenerator
+    {
+        public Guid RandomGuid { get; set; } = Guid.NewGuid();
+    }
+}
---
+++ IocContainerHW/SomeServiceOne.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IocContainerHW
+{
+    public class SomeServiceOne : ISomeService
+    {
+        private readonly IRandomGuidProvider _randomGuidProvider;
+
+        public SomeServiceOne(IRandomGuidProvider randomGuidProvider)
+        {
+            _randomGuidProvider = randomGuidProvider;
+        }
+
+        public void PrintSomething()
+        {
+            Console.WriteLine(_randomGuidProvider.RandomGuid);
+        }
+    }
+}
